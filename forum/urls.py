70a54bf5c7de968cd3dc6ffc022333ef5b6068fa
tusharsarkar3 <tushar.sarkar@somaiya.edu>
2022-03-23 11:47:24 +0530

    Forum

--- forum/urls.py
+++ forum/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.forum, name="Forum"),
-    path("discussion/<int:myid>/", views.replies, name="replies"),
+    path("", views.forum, name="forum"),
+    path("discussion/<int:myid>/reply", views.replies, name="replies"),
+    path("discussion/<int:myid>/comment", views.comment, name="comment"),
     path("discussion/<int:pk>/update", views.discussionUpdate, name="discussionUpdate"),
     path("discussion/<int:pk>/delete", views.discussionDelete, name="discussionDelete"),
-    path("discussion/<int:pk>/", views.replyUpdate, name="replyUpdate"),
+    #path("discussion/<int:pk>/", views.replyUpdate, name="replyUpdate"),
     #path("myprofile/", views.myprofile, name="Myprofile"),
 ]
--- forum/views.py
+++ forum/views.py
@@ -24,48 +24,64 @@ def forum(request):
     print(posts)
     return render(request, "forum.html", {'posts':posts})
 
+@login_required
+def comment(request,myid):
+    profile = Profile.objects.all()
+    post = Discussion.objects.filter(id=myid)
+    #print(posts)
+    replies = Reply.objects.filter(post=post[0])
+    return render(request, "comment.html", {'reply':replies,'posts':post})
+
 #@login_required(login_url = '/login')
 @login_required
 def replies(request, myid):
-    post = Discussion.objects.filter(id=myid).first()
+    post = Discussion.objects.get(id=myid)
     replies = Reply.objects.filter(post=post)
     if request.method=="POST":
         user = request.user
-        desc = request.POST.get('desc')
-        post_id =request.POST.get('post_id')
-        reply = Reply(user = user, reply_content = desc, post=post)
-        reply.save()
+        print(user)
+        rep = request.POST.get('reply')
+        print(rep)
+        post_id = myid
+        print(post)
+        print(post_id)
+        reply = Reply(user = user)
+        reply.reply_content = rep
+        reply.post = post
         print(reply)
-        #return render(request, "forum.html")
-    return render(request, "forum.html", {'post':post})
+        reply.save()
+        
+    return redirect('forum')
 
 
 @login_required
 def discussionUpdate(request,pk):
     disc=Discussion.objects.get(id=pk)
-    print(disc)
+    #print(disc)
     #user = User.objects.get(id = request.user.id)
     #print(user)
+    disc.delete()
+    print("in edit")
     if request.method == 'POST':
-        upd_form = DiscussionUpdateForm(request.POST)
-        if upd_form.is_valid():
-            a=upd_form.save(commit=False)
-            a.id=pk
-            a.save()
-            print(type(upd_form))
-            #user.profile= request.user.profile
-            posts = Discussion.objects.all()
-            #print(posts)
-            return render(request, "forum.html", {'posts':posts})
-
-    else:
-        upd_form = DiscussionUpdateForm(instance=request.user)
+        user=request.user
+        
+        title=request.POST.get('title')
+        content=request.POST.get('content')
+        d = Discussion(user1=user, disc_title=title,disc_content=content)
+        d.disc_content = content
+        d.disc_title = title
+        upd_form=d
+        d.save()
+        #upd_form = DiscussionUpdateForm(request.POST)
+        print(upd_form.is_valid())
+        
+        return redirect('forum')
 
     context = {
         'upd_form': upd_form
     }
 
-    return render(request, 'DiscussionUpdate.html', context)
+    return render(request, "DiscussionUpdate.html",context)
 
 
 @login_required
@@ -94,5 +110,5 @@ def discussionDelete(request,pk):
     disc.delete()
     posts = Discussion.objects.all()
             #print(posts)
-    return render(request, "forum.html", {'posts':posts})
+    return redirect('forum')
 
