4bd921d61284319968f6f4acf4da25edf59a151e
HarshitJain28 <harshit.nj@somaiya.edu>
2022-03-24 08:48:47 +0530

    Added councellor

--- counsellor/models.py
+++ counsellor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Councellor(models.Model):
+    email = models.EmailField()
+    address = models.TextField()
+    problem = models.TextField()
+
+    def __str__(self) -> str:
+        return self.email
--- counsellor/views.py
+++ counsellor/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Councellor
 
 # Create your views here.
+def counsellor(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        problem = request.POST.get('problem')
+        c = Councellor.objects.create(email=email,address=address,problem=problem)
+    return render(request, 'counsellor/councellor_connect.html')
