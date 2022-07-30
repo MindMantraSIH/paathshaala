19338e70d2d09a4bea291d1486f05ced9c7a2000
HarshitJain28 <harshit.nj@somaiya.edu>
2022-07-30 17:42:45 +0530

    Added Counsellor

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import School, Student, User
+from .models import School, Student, User, Counselor
 
 admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(User)
+admin.site.register(Counselor)
