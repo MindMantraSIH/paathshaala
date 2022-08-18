090c9a94d6c9139ba09dd6a74112163337e5d909
tusharsarkar3 <tushar.sarkar@somaiya.edu>
2022-08-18 23:09:14 +0530

    settings.py

--- paathshaala/settings.py
+++ paathshaala/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +151,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mindmantrasih@gmail.com'
 EMAIL_HOST_PASSWORD = 'qkvzybalfazzjigf'
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
