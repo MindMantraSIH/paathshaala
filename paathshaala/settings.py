df4ceb538d02ac25c97e98978ac063bdc9473c15
tusharsarkar3 <tushar.sarkar@somaiya.edu>
2022-08-18 23:55:45 +0530

    minor

--- paathshaala/settings.py
+++ paathshaala/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST_PASSWORD = 'qkvzybalfazzjigf'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 django_heroku.settings(locals())
