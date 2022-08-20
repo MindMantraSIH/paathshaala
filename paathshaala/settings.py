8c968c5e9e168e6ed3226f179421652f28222d14
HarshitJain28 <harshit.nj@somaiya.edu>
2022-08-20 19:35:38 +0530

    Counselor forum

--- paathshaala/settings.py
+++ paathshaala/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,4 +150,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mindmantrasih@gmail.com'
-EMAIL_HOST_PASSWORD = 'qkvzybalfazzjigf'
+EMAIL_HOST_PASSWORD = os.environ.get('paathshaala-mail')
+
