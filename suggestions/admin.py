b86fd79b7b5785ccebb8eae37de381f2063916da
HarshitJain28 <harshit.nj@somaiya.edu>
2022-08-26 05:25:59 +0530

    Standard to int

--- suggestions/admin.py
+++ suggestions/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Data)
+admin.site.register(Data1)
+admin.site.register(Data2)
+admin.site.register(Data3)
